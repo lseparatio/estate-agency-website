f4141bf3ceb667f25dcac6f5cd00369524270105
lseparatio <consultantaanglia@gmail.com>
2022-06-09 09:25:35 +0100

    Added Menu and started Sidebars

--- env_example.py
+++ env_example.py
@@ -2,7 +2,7 @@ import os
 
 os.environ.setdefault('SECRET_KEY', 'add a security key here')
 
-#Turn on of DEBUG. If True debug wil be On if False,
+#Turn on of DEBUG. If True debug wil be On,
 #Empty or anything else will be False.
 
 os.environ.setdefault('DEVELOPMENT', 'True')
--- home/views.py
+++ home/views.py
@@ -6,13 +6,4 @@ from django.contrib import messages
 
 def index(request):
     """A view to return the index page"""
-    messages.add_message(request, messages.DEBUG,
-                         'Salut asta este Un DEBUG Mesaj.')
-    messages.add_message(request, messages.INFO,
-                         'Salut asta este o informatie.')
-    messages.add_message(request, messages.SUCCESS,
-                         'Salut asta este o succes.')
-    messages.add_message(request, messages.WARNING,
-                         'Salut asta este o avertizare.')
-    messages.add_message(request, messages.ERROR, 'Salut asta este o eoare.')
     return render(request, 'home/index.html')
