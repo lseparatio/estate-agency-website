8c5b9f47abd98ec1e7dbc3c92a30a6d34d70c150
lseparatio <consultantaanglia@gmail.com>
2022-06-10 09:08:40 +0100

    Added database config variable

--- estate_agency/settings.py
+++ estate_agency/settings.py
@@ -125,10 +125,7 @@ WSGI_APPLICATION = 'estate_agency.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('DATABASE_URL')
 }
 
 
