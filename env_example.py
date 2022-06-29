1cc48856c4d4f9bf8c4c86aa648beb6d6ff062b8
lseparatio <consultantaanglia@gmail.com>
2022-06-29 17:02:50 +0100

    Updated env_example

--- env_example.py
+++ env_example.py
@@ -6,3 +6,6 @@ os.environ.setdefault('SECRET_KEY', 'add a security key here')
 #Empty or anything else will be False.
 
 os.environ.setdefault('DEVELOPMENT', 'True')
+
+os.environ.setdefault('DATABASE_URL', 'Database URL')
+os.environ.setdefault('ALLOWED_HOSTS', '127.0.0.1 for local development or your app link for online setup')
