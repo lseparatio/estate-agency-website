ef7ef27837da3cfce85539b562186088e6f20a05
lseparatio <consultantaanglia@gmail.com>
2022-08-04 09:14:52 +0100

    Added add products page with images preview

--- store/views.py
+++ store/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.contrib.auth.decorators import login_required
 from .models import Product, Category, Subcategory, Property_Type
+from .forms import ProductForm
 
 # Create your views here.
 
@@ -54,4 +56,31 @@ def product_detail(request, product_id):
         'product': product,
     }
 
-    return render(request, 'store/individual_product.html', context)
+    return render(request, 'store/individual-product.html', context)
+
+@login_required
+def add_product(request):
+    """ Add a product to the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            product = form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ProductForm()
+        
+    template = 'store/add-product.html'
+    context = {
+        'form': form,
+        'user': request.user,
+    }
+
+    return render(request, template, context)
+
